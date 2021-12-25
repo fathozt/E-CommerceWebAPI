06efb690ee6027503a04c4b238e1f757e112c0c0
Fatih Mehmet Öztürk <m.fath.oztt@outlook.com>
2021-12-25 13:48:11 +0100

    Add project files.

---
+++ Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Product : BaseEntity
+    {
+        public string CategoryName { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
